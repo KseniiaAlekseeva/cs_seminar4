a26c2554e9f6cb30f33392d0ad2f80b472fb8fae
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 21:05:09 +0300

    Add task28

---
+++ task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a function which counts the number of digits in a number
+
+int countDigits(int num)
+{
+    int count = 1;
+    int curNum = Math.Abs(num);
+    while (curNum > 9)
+    {
+        curNum = curNum / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Enter an integer number: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The number of digits in number {num} is {countDigits(num)}.");
+
+
---
+++ task28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Function calculates the multiplication of numbers in range [1,N]
+
+int multDigits(int num)
+{
+    int mult = 1;
+    for (int i = 2; i <= num; i++) mult = mult * i;
+    return mult;
+}
+
+int num = 0;
+
+while (num < 1)
+{
+    Console.Write("Enter a positive integer number: ");
+    num = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"The multiplication of numbers in range [1,{num}] is {multDigits(num)}.");
+
